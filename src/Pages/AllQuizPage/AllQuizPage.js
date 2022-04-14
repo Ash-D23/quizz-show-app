60aed141d524b70fca0fc10605656203e75b29d6
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-04-14 14:35:12 +0530

    feat: added single quiz cards

--- src/Pages/AllQuizPage/AllQuizPage.js
+++ src/Pages/AllQuizPage/AllQuizPage.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom';
+import SingleQuiz from '../../Components/SingleQuiz/SingleQuiz';
+import { db } from '../../firebase';
 
 function AllQuizPage() {
+
+  const [games, setgames] = useState([])
+
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    (async function(){
+      try{
+        const categories = db.ref('/Games');
+        const snapshot = await categories.once('value');
+        setgames(snapshot.val())
+      }catch(err){
+        console.error(err)
+      }
+    })()
+    
+  }, [])
+
+  const filterbyCategory = (arr, category) => {
+    if(!category){
+      return arr
+    }
+    return arr?.filter((item) => item.category == category)
+  }
+
+  const filterbySearch = (arr, search) => {
+    if(!search){
+      return arr
+    }
+    return arr?.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+  }
+
+  const filterGames = () => {
+    const category = searchParams.get('category')
+    const search = searchParams.get('search')
+    const filteredByCategory = filterbyCategory(games, category)
+    return filterbySearch(filteredByCategory, search)
+  }
+
+  const filteredGames = filterGames()
+
   return (
-    <div>AllQuizPage</div>
+    <div class="padding--large">
+            <h2 class="text--center margin--medium category--heading text--light">Play Now</h2>
+
+            <div class="container__flex--center container__flex--wrap">
+                { filteredGames?.map((item) => <SingleQuiz key={item?.id} game={item} />)}
+            </div>
+    </div>
   )
 }
 
