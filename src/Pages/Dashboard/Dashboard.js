8d442c7bf3266da7afc6b570b19bb1794a81aeb7
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-04-16 10:42:33 +0530

    feat: added User Profile page

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard() {
     (async function(){
         setisLoading(true)
         try{
-          const categories = db.ref('/results/'+user.uid);
+          const categories = db.ref('/results/'+user?.uid);
           const snapshot = await categories.once('value');
           const value = Object.values(snapshot.val())
           setresults(value)
