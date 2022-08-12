813cb4d5b5972f11e19956a15aba96634c19eae1
Ashutosh Kumar <30368163+Ash-D23@users.noreply.github.com>
2022-08-12 17:04:42 +0530

    feat: added firebase test

--- src/test/ApiCalls/Auth.test.js
+++ src/test/ApiCalls/Auth.test.js
@@ -9,7 +9,7 @@ jest.mock('../../firebase', () => {
             },
             signInWithEmailAndPassword(email, password) {
                 return new Promise((resolve, reject) => {
-                if (password === 'sign' || password === 'key') {
+                if (email === "test@test.com" && password === "test123") {
                     resolve({
                         email: "test@test.com",
                         firstName: "test",
@@ -19,24 +19,34 @@ jest.mock('../../firebase', () => {
                         userName: "testddc bdf",
                       });
                 }
-                reject(Error('sign in error '));
+                reject(Error('sign in error'));
                 });
             },
             createUserWithEmailAndPassword(email, password) {
                 return new Promise((resolve, reject) => {
-                if (password === 'create' || password === 'key') {
-                    resolve({ name: 'createUser' });
+
+                const validateEmail = (email) => {
+                    return email.match(
+                        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    );
+                };
+
+                if (validateEmail(email) && password.length >= 6) {
+                    resolve({ 
+                        uid: "A01YCODqSug6aZsgLG6vdBQRopn1",
+                        email: "test@test.com"
+                     });
                 }
                 reject(Error('create user error '));
                 });
             }
         }
     }
-  });
+});
 
 describe("Authentication", () => {
     test("Login", async () => {
-        const results = await firebase.auth.signInWithEmailAndPassword("test@test.com", "sign")
+        const results = await firebase.auth.signInWithEmailAndPassword("test@test.com", "test123")
         
         expect(results).toEqual({
             email: "test@test.com",
@@ -45,19 +55,21 @@ describe("Authentication", () => {
             lastName: "test",
             uid: "A01YCODqSug6aZsgLG6vdBQRopn1",
             userName: "testddc bdf",
-          })
+        })
     });
 
     test("SignUp", async () => {
-        const results = await firebase.auth.createUserWithEmailAndPassword("test@test.com", "create")
+        const results = await firebase.auth.createUserWithEmailAndPassword("test@test.com", "test123")
         
-        expect(results).toEqual({ name: 'createUser' })
+        expect(results).toEqual({ 
+            uid: "A01YCODqSug6aZsgLG6vdBQRopn1",
+            email: "test@test.com"
+        })
     });
 
     test("SignOut", async () => {
         const results = await firebase.auth.signOut()
         
-        expect("success").toEqual(results)
+        expect(results).toEqual("success")
     });
-
 })
--- src/test/functions/GameFilters.test.js
+++ src/test/functions/GameFilters.test.js
@@ -1,10 +1,16 @@
 import { filterbyCategory, filterbySearch } from "../../Utilities";
+import { mockGamesData } from '../mockData/mockData'
 
 
 describe("testing Quiz Game Filter", () => {
+    
     test("Category Filter", () => {
         
-        const arr = [{
+        const arr = mockGamesData
+
+        const category = "0"
+
+        const finalResult = [{
             category: 0,
             data: {
                 answers: [0,2],
@@ -20,77 +26,6 @@ describe("testing Quiz Game Filter", () => {
             id: 0,
             img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
             name: "Sherlock",
-        },
-        {
-            category: 1,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Skyfall",
-        },
-        {
-            category: 2,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "IPL",
-        },
-        {
-            category: 3,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Politics",
-        }]
-
-        const category = "1"
-
-        const finalResult = [{
-            category: 1,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Skyfall",
         }]
 
         let result = filterbyCategory(arr, category)
@@ -100,74 +35,7 @@ describe("testing Quiz Game Filter", () => {
 
     test("Search Filter", () => {
         
-        const arr = [{
-            category: 0,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Sherlock",
-        },
-        {
-            category: 1,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Skyfall",
-        },
-        {
-            category: 2,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "IPL",
-        },
-        {
-            category: 3,
-            data: {
-                answers: [0,2],
-                questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
-                },
-                { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
-                }]
-            },
-            id: 0,
-            img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
-            name: "Politics",
-        }]
+        const arr = mockGamesData
 
         const search = "skyfall"
 
@@ -176,15 +44,18 @@ describe("testing Quiz Game Filter", () => {
             data: {
                 answers: [0,2],
                 questions: [{ 
-                    name: 'Who Played the lead Role in TV Series Sherlock ?', 
-                    options: ["Benedict Cumberbatch","Robert Downey Jr.","Chris Evans"]
+                    name: 'Who played the Lead Role in SkyFall ?', 
+                    options: [
+                        "Daniel Craig",
+                        "Pierce Brosnan",
+                        "Sean Connery"]
                 },
                 { 
-                    name: 'Sherlock has how many siblings ?', 
-                    options: ["1","3","2"]
+                    name: 'At the end of the movie, Silva pursues M and gamekeeper Kincade to an old chapel. After pushing Kincade away, what does he want M to do?', 
+                    options: ["Commit suicide so Silva won't have to kill her himself", "Kill Bond", "Shoot them both"]
                 }]
             },
-            id: 0,
+            id: 1,
             img: "https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
             name: "Skyfall",
         }]
